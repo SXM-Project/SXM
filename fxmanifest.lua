eb4ab7c741628b44667c60d7196f7f2204b8c463
bub.bl <bub.bl@outlook.com>
2024-05-20 17:14:01 +0200

    Publish resource instead of code only

---
+++ fxmanifest.lua
@@ -0,0 +1,34 @@
+fx_version 'cerulean'
+game 'gta5'
+-- rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'bub.bl'
+description 'An awesome, but short, description'
+version '1.0.0'
+
+client_scripts {
+    "build/client/sxm-client.net.dll"
+}
+
+server_scripts {
+    "build/server/net8.0/sxm-server.net.dll"
+}
+
+files {
+    "build/deps/**/*.dll",
+    "build/client/**/*.dll",
+    "build/server/**/*.dll"
+}
+
+-- dependency "webpack"
+-- dependency "yarn"
+
+-- webpack_config "client.config.js"
+
+-- client_scripts {
+--     "build/client/index.js"
+-- }
+
+-- server_scripts {
+--     "build/server/index.js"
+-- }
